011e0051edc8da3514fe64f33fbe361378e1db46
niuyuhang03 <niuyuhang03@qq.com>
2019-06-14 20:05:58 +0800

    change correct

--- utils.py
+++ utils.py
@@ -101,27 +101,26 @@ def normalize_features(mx):
 def accuracy(output, labels, is_cuda):
     print("output:", output)
     preds = torch.zeros(labels.shape[0], labels.shape[1])
+    all_labels_1_length = 0
+    correct = 0
     for idx in range(len(labels)):
-        length = len(np.where(labels[idx])[0])
-        # print('labels_one_hot[idx]', labels_one_hot[idx])
-        # print("length", length)
-        # print('output[idx]:', output[idx])
-        predict_1_index = output[idx].sort()[1][-length:]
-        # print("output[idx].sort()", output[idx].sort()[0])
-        # print("output[idx].sort()[-length]", output[idx].sort()[0][-length])
-        # print("predict_1_index:", predict_1_index)
+        labels_1_index = np.where(labels[idx])[0]
+        labels_1_length = len(labels_1_index)
+        predict_1_index = output[idx].sort()[1][-labels_1_length:]
+        all_labels_1_length += labels_1_length
+        for labels_1 in labels_1_index:
+            if labels_1 in predict_1_index:
+                correct += 1
+        # 生成预测结果preds，仅查看用
         output_01 = np.zeros(labels.shape[1], dtype=np.float32)
         for i in predict_1_index:
             output_01[i] = 1.0
         preds[idx] = torch.from_numpy(output_01)
-        # print("preds[idx]:", preds[idx])
     if is_cuda:
         preds = preds.cuda()
     preds = preds.type_as(labels)
     print("preds:", preds)
-    correct = preds.eq(labels).double()
-    correct = correct.sum()
-    return correct / (labels.shape[0] * labels.shape[1])
+    return correct / all_labels_1_length
 
 
 # adj, features, labels_one_hot, idx_train, idx_val, idx_test, nclass = load_data(path='./data/FB15K237/', dataset='FB15K237')
