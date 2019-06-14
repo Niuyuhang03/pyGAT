79bec1aee9c46d3792a295b986eabcd006340f5b
niuyuhang03 <niuyuhang03@qq.com>
2019-06-14 20:39:08 +0800

    try to output sigmoid

--- train.py
+++ train.py
@@ -88,12 +88,13 @@ def train(epoch):
     optimizer.zero_grad()
     output = model(features, adj)
 
-    # loss_fn = torch.nn.BCELoss(reduce=True, size_average=True)
-    # sigmoid_fn = torch.nn.Sigmoid()
-    # loss_train = loss_fn(sigmoid_fn(output[idx_train]), labels[idx_train].type_as(output[idx_train]))
+    # loss_fn = nn.BCELoss(reduce=True, size_average=True)
+    # sigmoid_fn = nn.Sigmoid()
+    # sigmoid_output_idx_train = sigmoid_fn(output[idx_train])
+    # loss_train = loss_fn(sigmoid_output_idx_train, labels[idx_train].type_as(output))
 
-    loss_fn = torch.nn.BCEWithLogitsLoss(reduce=True, size_average=True)
-    loss_train = loss_fn(output[idx_train], labels[idx_train].type_as(output[idx_train]))
+    loss_fn = nn.BCEWithLogitsLoss(reduce=True, size_average=True)
+    loss_train = loss_fn(output[idx_train], labels[idx_train].type_as(output))
 
     # loss_train = F.nll_loss(output[idx_train], labels[idx_train])
 
@@ -107,12 +108,13 @@ def train(epoch):
         model.eval()
         output = model(features, adj)
 
-    # loss_fn = torch.nn.BCELoss(reduce=True, size_average=True)
-    # sigmoid_fn = torch.nn.Sigmoid()
-    # loss_val = loss_fn(sigmoid_fn(output[idx_val]), labels[idx_val].type_as(output[idx_train]))
+    # loss_fn = nn.BCELoss(reduce=True, size_average=True)
+    # sigmoid_fn = nn.Sigmoid()
+    # sigmoid_output_idx_val = sigmoid_fn(output[idx_val])
+    # loss_val = loss_fn(sigmoid_output_idx_val, labels[idx_val].type_as(output))
 
-    loss_fn = torch.nn.BCEWithLogitsLoss(reduce=True, size_average=True)
-    loss_val = loss_fn(output[idx_val], labels[idx_val].type_as(output[idx_train]))
+    loss_fn = nn.BCEWithLogitsLoss(reduce=True, size_average=True)
+    loss_val = loss_fn(output[idx_val], labels[idx_val].type_as(output))
 
     # loss_val = F.nll_loss(output[idx_val], labels[idx_val])
     
@@ -135,12 +137,13 @@ def compute_test():
 
     # loss_test = F.nll_loss(output[idx_test], labels[idx_test])
 
-    # loss_fn = torch.nn.BCELoss(reduce=True, size_average=True)
-    # sigmoid_fn = torch.nn.Sigmoid()
-    # loss_test = loss_fn(sigmoid_fn(output[idx_test]), labels[idx_test].type_as(output[idx_train]))
+    # loss_fn = nn.BCELoss(reduce=True, size_average=True)
+    # sigmoid_fn = nn.Sigmoid()
+    # sigmoid_output_idx_test = sigmoid_fn(output[idx_test])
+    # loss_test = loss_fn(sigmoid_output_idx_test, labels[idx_test].type_as(output))
 
-    loss_fn = torch.nn.BCEWithLogitsLoss(reduce=True, size_average=True)
-    loss_test = loss_fn(output[idx_test], labels[idx_test].type_as(output[idx_train]))
+    loss_fn = nn.BCEWithLogitsLoss(reduce=True, size_average=True)
+    loss_test = loss_fn(output[idx_test], labels[idx_test].type_as(output))
 
     acc_test = accuracy(output[idx_test], labels[idx_test], args.cuda)
     print("Test set results:",
--- try.py
+++ try.py
@@ -16,29 +16,28 @@ loss_fn = nn.BCEWithLogitsLoss()
 loss_train = loss_fn(output, labels)
 print("loss_train", loss_train)
 
-print("labels", labels)
-print("output:", output)
-
 def accuracy(output, labels):
+    print("labels", labels)
+    print("output:", output)
     preds = torch.zeros(labels.shape[0], labels.shape[1])
+    all_labels_1_length = 0
+    correct = 0
     for idx in range(len(labels)):
-        length = len(np.where(labels[idx])[0])
-        # print('labels_one_hot[idx]', labels_one_hot[idx])
-        # print("length", length)
-        # print('output[idx]:', output[idx])
-        predict_1_index = output[idx].sort()[1][-length:]
-        # print("output[idx].sort()", output[idx].sort()[0])
-        # print("output[idx].sort()[-length]", output[idx].sort()[0][-length])
+        labels_1_index = np.where(labels[idx])[0]
+        labels_1_length = len(labels_1_index)
+        print('labels_1_index:', labels_1_index)
+        predict_1_index = output[idx].sort()[1][-labels_1_length:]
+        print('predict_1_index:', predict_1_index)
+        all_labels_1_length += labels_1_length
+        for labels_1 in labels_1_index:
+            if labels_1 in predict_1_index:
+                correct += 1
+        # 生成预测结果preds，仅查看用
         output_01 = np.zeros(7, dtype=np.float32)
         for i in predict_1_index:
             output_01[i] = 1.0
         preds[idx] = torch.from_numpy(output_01)
-        # print("preds[idx]:", preds[idx])
     preds = preds.type_as(labels)
     print("preds:", preds)
-    correct = preds.eq(labels).double()
-    print("correct:", correct)
-    correct = correct.sum()
-    print("correct.sum:", correct)
-    return correct / labels.shape[0] / labels.shape[1]
+    return correct / all_labels_1_length
 print("accuracy:", accuracy(output, labels))
