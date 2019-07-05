50a4a35fd3a0827b598c24af0a66f34f1971f163
niuyuhang03 <niuyuhang03@qq.com>
2019-07-05 19:46:04 +0800

    add edge

--- layers.py
+++ layers.py
@@ -60,3 +60,38 @@ class GraphAttentionLayer(nn.Module):
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+
+class GraphEdgeAttentionLayer(nn.Module):
+    """
+    Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
+    """
+
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True, residual=False):
+        super(GraphEdgeAttentionLayer, self).__init__()
+        self.dropout = dropout
+        self.in_features = in_features
+        self.out_features = out_features
+        self.alpha = alpha
+        self.concat = concat
+        self.residual = residual
+
+        self.f_1 = nn.Conv1d(out_features, 1, kernel_size=1, stride=1)
+
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+
+    def forward(self, input, adj):
+        # Too harsh to use the same dropout. TODO add another dropout
+        # input = F.dropout(input, self.dropout, training=self.training)
+
+        f_1 = self.f_1(input) # a1
+        coefs = F.softmax(self.leakyrelu(f_1) + adj, dim=1)
+
+        ret = F.dropout(coefs, self.dropout, training=self.training)
+
+        if self.concat:
+            return F.elu(ret)
+        else:
+            return ret
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
--- models.py
+++ models.py
@@ -1,12 +1,36 @@
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
-from layers import GraphAttentionLayer
+from layers import GraphAttentionLayer, GraphEdgeAttentionLayer
 
 
 class GAT(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
         super(GAT, self).__init__()
+        self.dropout = dropout
+
+        self.attentions = [GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True) for _ in range(nheads)]
+        for i, attention in enumerate(self.attentions):
+            self.add_module('attention_{}'.format(i), attention)
+
+        self.out_att = GraphAttentionLayer(nhid * nheads, nhid * nheads, dropout=dropout, alpha=alpha, concat=False)
+        self.linear_att = nn.Linear(nhid * nheads, nclass)
+
+    def forward(self, x, adj):
+        # 学习K个不同的attention，对应参数aij^k，W^k，然后在生成节点i的新特征时拼接起来：
+        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
+
+        # 在整个图神经网络的最后一层，使用平均替代拼接，得到节点最终的embedding
+        x = self.out_att(x, adj)
+        # 增加一个全连接层
+        x = self.linear_att(x)
+        return F.log_softmax(x, dim=1)
+        # sigmoid_fn = nn.Sigmoid()
+        # return sigmoid_fn(x)
+
+class GAT_edge(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
+        super(GAT_edge, self).__init__()
         self.dropout = dropout
 
         self.attentions = [GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True) for _ in range(nheads)]
