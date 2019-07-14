c91f8f687c9d9ec7d0a267a74faadffdcb0c9983
niuyuhang03 <niuyuhang03@qq.com>
2019-07-15 00:09:43 +0800

    fix bugs about .pkl dir

--- train.py
+++ train.py
@@ -124,6 +124,7 @@ loss_values = []
 bad_counter = 0
 best = args.epochs + 1
 best_epoch = 0
+os.mkdir('{}'.format(args.experiment))
 
 for epoch in range(args.epochs):
     loss_values.append(train(epoch))
